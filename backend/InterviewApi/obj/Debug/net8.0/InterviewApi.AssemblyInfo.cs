4963ef8152fe9603aa560b49974af8d59b2ff4ed
Alex Pahomi <alexpahomi.ap@gmail.com>
2025-10-21 10:42:13 +0300

    Remove backend obj from git

--- backend/InterviewApi/obj/Debug/net8.0/InterviewApi.AssemblyInfo.cs
+++ backend/InterviewApi/obj/Debug/net8.0/InterviewApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterviewApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd8863d569c6cb973af99b16551e35ec03603ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15208f483c533153712f6b394d9fc82b1b46276")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterviewApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterviewApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
